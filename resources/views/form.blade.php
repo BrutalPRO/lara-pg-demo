a66567a7073d56c2f31664405748dca949262bb0
BrutalPRO <bmkobzar@gmail.com>
2022-11-19 11:29:45 +0200

    dev

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -2,15 +2,15 @@
     <form action="{{route('point.add')}}" method="post">
         @csrf
         <div>
-            <label>Latitude*:<br>
+            <label>Latitude*:
             <input type="number" name="latitude" step="0.000001" min="-90" max="90" required="required" /></label>
         </div>
         <div>
-            <label>Longitude*:<br>
+            <label>Longitude*:
             <input type="number" name="longitude" step="0.000001" min="-180" max="180" required="required" /></label>
         </div>
         <div>
-        <label>Label:<br>
+        <label>Label:
             <input type="text" name="label"></label>
         </div>
         <div>
