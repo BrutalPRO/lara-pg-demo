0f2af67e74866df19f29053c99bf32fdb24c0485
BrutalPRO <bmkobzar@gmail.com>
2022-11-19 15:45:00 +0200

    dev

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,21 +7,30 @@
         <title>Laravel PostgreSql demo task</title>
         <style>
             html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}
+            html,body{min-height: 100%;padding: 0;margin: 0}
             #map{
                 width: 100%;
                 height: 460px;
             }
-            .container{width: 90%; max-width: 1440px;}
+            .container{width: 90%; max-width: 1440px; margin: 0 auto;}
             .form{
-                background-color: rgb(243, 244, 246);
+                background-color: #fff;
                 padding: 1.5em;
-                border: 1px solid #e5e7eb;
+                border: 1px solid rgb(229, 231, 235);
                 border-radius: 15px;
             }
+            @media (min-width: 768px) {
+                .form{
+                    display: flex;
+                    justify-content: space-between;
+                }
+            }
+            label{margin: 15px 0;}
         </style>
     </head>
     <body>
     <div class="container">
+        <h1>PHP Developer Test Task</h1>
         @include('form')
         <div id="map"></div>
     </div>
